5759fe36a4d7f3b3f4e32b4b5814f14843f8c453
dragosdragos935 <dragosdragos935@gmail.com>
2025-02-11 21:43:51 +0200

    Filter blogs by category

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client'
 
 import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 
@@ -8,7 +9,7 @@ export default function Home() {
   return (
    <>
     <Header/>
-    <BlogItem/>
+    <BlogList/>
    </>
   );
 }
